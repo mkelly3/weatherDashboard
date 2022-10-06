04fc424d64e2225792bf7076f629b9bffb4a895e
Morgan Kelly <mkelly5380@gmail.com>
2022-10-06 11:51:49 -0500

    taking inputed city and setting it as the header in main

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-rootEl = $('header');
-currentDayEl = $('#currentDay');
+// rootEl = $('header');
+// currentDayEl = $('#currentDay');
 
-function showProject() {
-    var timerId = setInterval(function() {
-        currentDayEl.text(moment().format('dddd, MMMM Do hh:mm:ss'));
-        /* never clear this interval ? */
-    }, 1000);
+// function showProject() {
+//     var timerId = setInterval(function() {
+//         currentDayEl.text(moment().format('dddd, MMMM Do hh:mm:ss'));
+//         /* never clear this interval ? */
+//     }, 1000);
+// }
+
+// $('document').ready(showProject);
+
+
+function displayCity(){
+
+    $(".btn").click(function(){
+        $('.cityName').text($('.cityInput').val());
+        console.log("clicked");
+        console.log($('.cityInput').val());
+    });
+    
 }
 
-$('document').ready(showProject);
+displayCity();
